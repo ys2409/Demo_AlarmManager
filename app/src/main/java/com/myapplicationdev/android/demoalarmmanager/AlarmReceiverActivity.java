1f1b078183489c6c693a385ea34b617599765dfd
ys2409 <19022397@myrp.edu.sg>
2021-08-03 11:33:39 +0800

    lesson11

--- app/src/main/java/com/myapplicationdev/android/demoalarmmanager/AlarmReceiverActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoalarmmanager/AlarmReceiverActivity.java
@@ -1,4 +1,23 @@
 package com.myapplicationdev.android.demoalarmmanager;
 
-public class AlarmReceiverActivity {
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+public class AlarmReceiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_receiver);
+
+    }
+
 }
